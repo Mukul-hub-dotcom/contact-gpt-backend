d01a403b8a52d5fdf14d1abd6c399e151635ba61
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-04-07 18:25:27 +0530

    added mongodb url

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB Configuration
-const uri = '';
-mongoose.connect('mongodb://localhost:27017/contacto', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://apida:apida@project.mge1yrz.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
